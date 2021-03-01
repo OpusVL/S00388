cdb66981780fabacf263ccee201e352ee0cda474
Paul Bargewell <paul.bargewell@opusvl.com>
2021-03-01 12:26:41 +0000

    docker permissions

--- invoicing/deploy.sh
+++ invoicing/deploy.sh
@@ -12,10 +12,14 @@ function render_template() {
   eval "echo \"$(cat $1)\""
 }
 
-chown ../ ${DIR} -R
+chown ubuntu ../ -R
 chgrp docker ../ -R
 
+# Render the odoo.conf template file 
+
 render_template odoo.conf.tpl > ./odoo/etc/odoo.conf
+
+# Enable login to the registry without username / password
 
 mkdir -p ~/.docker
 render_template config.json.tpl > ~/.docker/config.json
@@ -25,15 +29,19 @@ chmod u+rw,g-rwx,o-rwx ~/.docker
 
 # Add the user to the docker group
 
-gpasswd -a ${USER} docker
+gpasswd -a ubuntu docker
+
+# Start syslog - if you use tcp then this MUST be running or the Odoo container wont start
 
 cd ../syslog-ng || exit 1
 
-docker-compose run --rm -u root syslog chown root: /etc/logrotate.d/ -R
-
 ln -s ../invoicing/.env .env
 
+docker-compose run --rm -u root syslog chown root: /etc/logrotate.d/ -R
+
 docker-compose up -d
+
+# Start Odoo
 
 cd ../invoicing || exit 1
 
